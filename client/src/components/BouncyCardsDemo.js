a524c07a8e058a5016706204fe7b8666f5e18250
ben <benyamin.1983@hotmail.com>
2025-10-31 16:18:51 -0400

    Add: BouncyCardsFeatures component for animated feature showcase

---
+++ client/src/components/BouncyCardsDemo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BouncyCardsFeatures } from './ui/bounce-card-features';
+
+/**
+ * Demo component showing the BouncyCardsFeatures in action
+ * This can be used as an example or integrated into pages as needed
+ */
+const DemoOne = () => {
+  return <BouncyCardsFeatures />;
+};
+
+export { DemoOne };
+
