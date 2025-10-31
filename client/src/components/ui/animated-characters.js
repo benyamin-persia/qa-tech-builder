d684dc034aa4a327cf2d88e87a4b964e85f5dd28
ben <benyamin.1983@hotmail.com>
2025-10-31 15:39:45 -0400

    Fix: Make black character even taller

--- client/src/components/ui/animated-characters.js
+++ client/src/components/ui/animated-characters.js
@@ -166,10 +166,10 @@ const AnimatedCharacters = ({ className }) => {
       try {
         return JSON.parse(savedSizes);
       } catch (e) {
-        return { purple: 133, black: 124, orange: 67, yellow: 45 };
+        return { purple: 133, black: 148, orange: 67, yellow: 45 };
       }
     }
-    return { purple: 133, black: 124, orange: 67, yellow: 45 };
+    return { purple: 133, black: 148, orange: 67, yellow: 45 };
   });
   const [purpleLeftEyePosition, setPurpleLeftEyePosition] = useState({ x: 0, y: 0 });
   const purpleRef = useRef(null);
